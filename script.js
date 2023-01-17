c877c294a00d6ac67d09e07dbbb68532637839fc
tyVespA <migliavacca.marco08@gmail.com>
2023-01-17 11:00:33 +0100

    Add adding and displaying books functions

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const addBookBtn = document.querySelector(".add-book");
 const submitBookBtn = document.querySelector("#submit-book");
 const modal = document.querySelector(".modal");
+const booksGrid = document.querySelector(".books-grid");
 let myLibrary = [];
 let LatestBook;
 
@@ -8,6 +9,11 @@ let title;
 let author;
 let pages;
 let readStatus;
+
+addBookBtn.addEventListener("click", () => {
+  modal.classList.toggle("active");
+  // addBookToLibrary();
+});
 
 function Book(title, author, pages, readStatus) {
   this.title = title;
@@ -28,25 +34,40 @@ function getBookInfo() {
   pages = document.getElementById("pages").value;
   readStatus = document.getElementById("read-status").value;
 
-  LatestBook = new Book(title, author, pages, readStatus);
-  return LatestBook;
+  return (LatestBook = new Book(title, author, pages, readStatus));
 }
 
 function addBookToLibrary() {
   getBookInfo();
   myLibrary.push(LatestBook);
-  console.log(LatestBook);
+  return myLibrary;
 }
 
-addBookBtn.addEventListener("click", () => {
-  modal.classList.toggle("active");
-  // addBookToLibrary();
-});
+function displayLatestBook(myLibrary) {
+  for (let book of myLibrary) {
+    // booksGrid.removeChild(bookDiv);
+    const bookDiv = document.createElement("div");
+    bookDiv.classList.add("book");
+    booksGrid.appendChild(bookDiv);
+    const printedTitle = document.createElement("p");
+    bookDiv.appendChild(printedTitle);
+    printedTitle.textContent = "Title: " + book.title;
+
+    const printedAuthor = document.createElement("p");
+    bookDiv.appendChild(printedAuthor);
+    printedAuthor.textContent = "Author: " + book.author;
+
+    const printedPages = document.createElement("p");
+    bookDiv.appendChild(printedPages);
+    printedPages.textContent = "Pages: " + book.pages;
+  }
+}
 
 submitBookBtn.addEventListener("click", (e) => {
   e.preventDefault();
   modal.classList.remove("active");
-  getBookInfo();
-  myLibrary.push(LatestBook);
+  booksGrid.replaceChildren();
+  addBookToLibrary();
+  displayLatestBook(myLibrary);
   console.log(myLibrary);
 });
